c9c057a37ce629ecb577d88b114e0df8201b1cd6
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2022-06-28 17:44:28 -0500

    inital commit

---
+++ src/config/conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Samir, Darwin Hincho
+ * BD_LAB06 EPIS UNSA
+ */
+public class conexion {
+    Connection connection = null;
+    public conexion(){
+        
+        try{
+            String user = "root";
+            String password = "";
+            String url = "jdbc:mysql://localhost:7777/lab05";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,user,password);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
